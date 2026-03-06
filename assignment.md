ce0d76df3572bfbf3d433763e9b49e89031f4f2e
Kolade Amire <stephamire@gmail.com>
2026-03-06 21:43:05 +0100

    project init: assignment 1

---
+++ assignment.md
@@ -0,0 +1,21 @@
+## Assignment: Student Result Summary
+
+- File to create: student-result.js
+- Requirements: Store student name and score in variables.
+- Grading Rules:
+70 and above = A
+60 to 69 = B
+50 to 59 = C
+40 to 49 = D
+Below 40 = F
+
+Pass Rules:
+50 and above = Pass. Below 50 = Fail.
+
+**Example Expected Output:**
+`
+Student Name: Amina
+Score: 72
+Grade: A
+Status: Pass
+`
---
+++ index.js
@@ -0,0 +1,58 @@
+
+// Basic Data Types in JavaScript
+/**
+- string
+- number
+- boolean -> true | false
+- null
+- undefined
+**/
+
+
+let fullName = "Donald trump";
+let score = 90.2;
+let passed = true;
+let state = null;
+let middleName;
+
+//Operators in JS
+
+let a = 10;
+let b = "10";
+
+//arithmetic operators
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+//operators for comparisons
+//they resolve to true or false
+
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a === b);
+
+//Conditional Statement
+// let age = 16;
+
+//syntax of if-else statement
+
+// if (age <= 16) {
+//     console.log("Admission faled!");
+// } else {
+//     console.log("Admission successful!");
+// }
+
+const name = "Tunde";
+const age = 32;
+const isCorper = true;
+
+
+// AND / OR
+if (age < 30 && isCorper == true) {  // ||
+    console.log(nam + " is eligible for 77k");
+} else {
+    console.log(nam + " is not eligible");
+}
+
